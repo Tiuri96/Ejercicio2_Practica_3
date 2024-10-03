f7c7196968d89a759cc0248c255e9e91d35abb6c
Tiuri96 <linternaroja2006@gmail.com>
2024-10-03 18:40:28 +0200

    Add files via upload

---
+++ ej5.py
@@ -0,0 +1,8 @@
+número = float(input('Inserte el número de grados (Fahrenheit): '))
+
+print('Hay', número, 'grados (Fahrenheit)')
+
+celsius = (número - 32) * (5 / 9)
+
+
+print('Hay un total de ', celsius, 'grados (Celsius)')
